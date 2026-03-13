d32ab77b6385de0c67a308ab48b708c9f937d064
Kaushikmandal2580 <kaushik135790@gmail.com>
2026-03-13 16:47:15 +0530

    a

---
+++ Veriables and Data Type.py
@@ -0,0 +1,15 @@
+print("HELLO WORLD")
+print("2580")
+name = "kaushik"
+age = 25
+price = 25.222
+print (name)
+print ("name")
+print ("MY name is",name)
+name2 = name
+print(name2)
+print(type(name))
+print(type(age))
+print(type(price))
+name1 ="k"
+name2='k'
